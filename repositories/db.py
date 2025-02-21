8d2d0ef355e5de2915b661edf9e4e1b1b560ed94
Kendall_Tart <kenbazonct14@gmail.com>
2025-02-21 18:23:48 -0500

    debug

--- repositories/db.py
+++ repositories/db.py
@@ -2,11 +2,9 @@ import os
 from psycopg_pool import ConnectionPool
 
 pool = None
-
+conninfo=os.getenv('DATABASE_URL')
 def get_pool():
     global pool
     if not pool:
-        pool = ConnectionPool(
-            conninfo=os.getenv('DATABASE_URL'),
-        )
+        pool = ConnectionPool(conninfo)
     return pool
