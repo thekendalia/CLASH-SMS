de65035146c397d72a1e0d1bc7bb8dc4c23e6c85
Kendall_Tart <kenbazonct14@gmail.com>
2025-02-21 19:18:49 -0500

    debug

--- otherpy/keep_alive.py
+++ otherpy/keep_alive.py
@@ -8,7 +8,7 @@ def home():
     return "Bot is running!"
 
 def run():
-    app.run(host="0.0.0.0", port=8080)
+    app.run(host="0.0.0.0", port=5000)
 
 def keep_alive():
     thread = Thread(target=run)
--- repositories/db.py
+++ repositories/db.py
@@ -6,7 +6,6 @@ load_dotenv()
 
 pool = None
 conninfo=os.getenv('DATABASE_URL')
-print(conninfo)
 def get_pool():
     global pool
     if not pool:
