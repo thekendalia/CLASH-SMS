d2b313dff646c5d2e671229d1e749e8ec21b2dc7
Kendall_Tart <kenbazonct14@gmail.com>
2025-02-21 19:23:15 -0500

    debug

--- app.py
+++ app.py
@@ -706,5 +706,5 @@ def disabled():
 
 if __name__ == '__main__':  
     # Get the port from the environment variable or use a default value  
-    port = int(os.environ.get('PORT', 5000))  
+    port = int(os.environ.get('PORT', 10000))  
     app.run(host='0.0.0.0', port=port)  
